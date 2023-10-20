5c7e40b2529a03604428a31fc39aee1bfae271b4
Franjhe <franjhely.andre13@gmail.com>
2023-10-20 15:13:26 -0400

    timeoutrequest

--- src/db/AdditionalCharges.js
+++ src/db/AdditionalCharges.js
@@ -4,7 +4,9 @@ const sqlConfig = {
     user: process.env.USER_BD,
     password: process.env.PASSWORD_BD,
     server: process.env.SERVER_BD,
-    database: process.env.NAME_BD,
+        database: process.env.NAME_BD,
+    requestTimeout: 60000, 
+    requestTimeout: 60000, 
     options: {
         encrypt: true,
         trustServerCertificate: true
--- src/db/PaymentInstallments.js
+++ src/db/PaymentInstallments.js
@@ -5,7 +5,8 @@ const sqlConfig = {
     user: process.env.USER_BD,
     password: process.env.PASSWORD_BD,
     server: process.env.SERVER_BD,
-    database: process.env.NAME_BD,
+        database: process.env.NAME_BD,
+    requestTimeout: 60000, 
     options: {
         encrypt: true,
         trustServerCertificate: true
